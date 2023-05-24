27d5cf2475e3553f67efcd980049d9876fb95d1e
cukwak <31978182+cukwak@users.noreply.github.com>
2023-05-25 00:47:00 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>Server Management</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div id = main ><br>
+        <div id = main-1>
+            Sever Management
+        </div><br>
+        <a href = "register.html">        
+            <input type = "button" value="register" onclick="alert">
+        </a>
+        <a href="login.html">
+            <input type = "button" value="log in" onclick="alert">
+        </a>
+    </div> 
+</body>
+</html>
