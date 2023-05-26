303bfe0696b11394c0dd7486f1b734785dfeca07
cukwak <31978182+cukwak@users.noreply.github.com>
2023-05-27 02:27:55 +0900

    Add files via upload

--- postpc.php
+++ postpc.php
@@ -1,3 +1,18 @@
 <?php
-var_dump($_POST);
+$conn = mysqli_connect('localhost', 'test', '1234','test');
+
+$sql = "INSERT INTO post(title, description created)
+        VALUES '{$_POST['title']}',
+                '{$_POST['description']}',
+                NOW()
+        )";
+$result = mysqli_query($conn,$sql);
+if($result == false){
+    echo "<script>
+            alert('post failed');
+          </script>";
+}else{
+    echo"<script> location.replace('index.php'); </script>;";
+}
+echo $sql;
 ?>
